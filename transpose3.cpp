fe36fcf4ae611501bdc803a7135a77034620af76
DewiNurArifah <72842638+DewiNurArifah@users.noreply.github.com>
2021-02-16 10:46:05 +0700

    Add files via upload

---
+++ transpose3.cpp
@@ -0,0 +1,32 @@
+/*DEWI NUR ARIFAH
+NIM : 20051397079
+KELAS : 2020A */
+// Program Transpose
+
+#include <iostream>
+using namespace std;
+int main(){
+  int i, j, m, n, matriks[10][10], transpose[10][10];
+  cout << "Masukkan jumlah baris matriks: ";
+  cin >> m;
+  cout << "Masukkan jumlah kolom matriks: ";
+  cin >> n;
+  cout << "Masukkan elemen matriks\n";
+  for (i = 0; i < m; i++){
+    for (j = 0; j < n; j++){
+      cin  >> matriks[i][j];
+    }
+  }
+  for (i = 0; i < m; i++){
+    for (j = 0; j < n; j++){
+      transpose[j][i] = matriks[i][j];
+    }
+  }
+  cout << "Hasil Transpose Matriks: \n";
+  for (i = 0; i < n; i++){
+    for (j = 0; j < m; j++){
+      cout << transpose[i][j] << "\t";
+    }
+    cout << endl;
+  }
+}
